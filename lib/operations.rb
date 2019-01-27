e893b2403692bbdc7f0845ea557326a6928c779f
Yonnick Brown <25hour24@gmail.com>
2019-01-27 10:26:31 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,11 @@
 def unsafe?(speed)
-  if speed.between(40,60)
+  if speed.between?(40,60)
     true
   else
     false
   end
 end
 
-
-
 def not_safe?(speed)
-
+ speed.between?(40,60) ? true : false
 end
